032605901c6c0eb8b4dc77c4761627af8f79e71f
Maxence Leduc <maxence.leduc@yahoo.com>
2023-04-27 16:55:44 +0200

    starting products administration

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin/products', name: 'index_crud_products')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('admin/index.html.twig', []);
+        $products = $productRepository->findAll();
+
+        return $this->render('admin/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    #[Route('admin/products/{id}', name: 'product_admin')]
+    public function item(Product $product): Response
+    {
+        return $this->render('admin/item.html.twig', [
+            'product' => $product
+        ]);
     }
 }
