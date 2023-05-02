30562beaa0e51b6fb94ceaf62db3313c69ecfc27
Maxence Leduc <maxence.leduc@yahoo.com>
2023-05-02 11:29:12 +0200

    delete option and try add option

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\AddProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,7 +23,7 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('admin/products/{id}', name: 'product_admin')]
+    #[Route('admin/products/read/{id}', name: 'product_admin')]
     public function item(Product $product): Response
     {
         return $this->render('admin/item.html.twig', [
@@ -31,9 +34,30 @@ class AdminController extends AbstractController
     #[Route('admin/products/delete/{id}', name: 'delete_product')]
     public function delete(Product $product, ProductRepository $productRepository): Response
     {
-        $productRepository->remove($product);
+        $productRepository->remove($product, true);
         $this->addFlash('success', 'Produit supprimé');
 
+        return $this->redirectToRoute('index_crud_products');
+    }
+
+    #[Route('admin/products/add', name: 'add_product', methods: ['GET', 'POST'])]
+    public function add(Request $request, EntityManagerInterface $em): Response
+    {
+        $product = new Product;
+        $form = $this->createForm(AddProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Le produit a bien été ajouté');
+
             return $this->redirectToRoute('index_crud_products');
+        }
+
+        return $this->renderForm('admin/create.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
